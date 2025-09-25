3d14de9a5a8035ccf9a002fba9c98a062f623635
aniyahthompsonn <athompson10@aggies.ncat.edu>
2025-09-25 12:19:25 -0400

    Pass Test Case 1: Initial game setup with required variables

---
+++ aniyahthompsonn_assignment_4.py
@@ -0,0 +1,13 @@
+student_name = "Aniyah Thompson"
+current_gpa = 3.8
+study_hours = 48
+social_points = 50
+stress_level = 65
+
+#welcome message and stats
+print ("Welcome to your Academic Profile!")
+print (f"Student Name: {student_name}")
+print (f"Current GPA: {current_gpa}")
+print (f"Study Hours: {study_hours}")
+print (f"Social Points: {social_points}")
+print (f"Stress Level: {stress_level}")
